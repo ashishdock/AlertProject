e0767868f1e9ae4b294581faa27308e8df550735
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-22 12:10:20 +0530

    First project commit

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordRepeatTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
 
-
+    @IBAction func signupButtonClicked(_ sender: UIButton) {
+        
+        // can make a function for alert and use it repeatedly in many other places in the app.
+        let alert = UIAlertController(title: "Error!", message: "Username not found!", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+            //button clicked
+            print("Alert OK button clicked.")
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+        //if usernameTextField.text = "" {
+            
+        //}
+    }
+    
 }
 
